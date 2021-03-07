776fd7a349bc8755f5530a419003b30c70fd3665
Denis Luchnikov <lasvegas3600@gmail.com>
2021-03-07 20:25:04 +0300

    Add errorsHandler

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const config = require('./config');
+
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message || 'enternal error',
+      status: err.status,
+    },
+  });
+});
 app.listen(config.PORT, () => {
   console.log(`Сервер работает на ${config.PORT} порту`);
 });
