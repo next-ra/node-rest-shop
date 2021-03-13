b466b86fb8ddf88747070b23152f83f16b005b7b
Denis Luchnikov <lasvegas3600@gmail.com>
2021-03-13 17:40:21 +0300

    Add orders controller

---
+++ api/controllers/orders.js
@@ -0,0 +1,95 @@
+const Order = require('../models/order');
+const Product = require('../models/product');
+const NotFound = require('../../customErrors/NotFound');
+exports.orders_get_all = async (req, res, next) => {
+  try {
+    const orders = await Order.find({})
+      .select('-__v')
+      .populate('product', 'name');
+    const response = {
+      count: orders.length,
+      orders: orders.map((o) => {
+        return {
+          ...o._doc,
+          request: {
+            description: 'Get order details',
+            type: 'GET',
+            url: `http://localhost:3000/orders/${o._id}`,
+          },
+        };
+      }),
+    };
+    res.status(200).json(response);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.orders_create_one = async (req, res, next) => {
+  try {
+    await Product.findById(req.body.product).orFail(
+      new NotFound('Продукт не найден'),
+    );
+    const { product, quantity } = req.body;
+    const order = await Order.create({
+      product,
+      quantity,
+    });
+    res.status(201).json({
+      message: 'Order was created',
+      order: {
+        ...order._doc,
+        request: {
+          description: 'Get order details',
+          type: 'GET',
+          url: `http://localhost:3000/orders/${order._id}`,
+        },
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.orders_get_one = async (req, res, next) => {
+  try {
+    const id = req.params.orderId;
+    const order = await Order.findById(id)
+      .select('-__v')
+      .populate('product')
+      .orFail(new NotFound('Order is not found'));
+    res.status(200).json({
+      order,
+      request: {
+        description: 'Get all orders',
+        type: 'GET',
+        url: `http://localhost:3000/orders`,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.orders_delete_one = async (req, res, next) => {
+  try {
+    const id = req.params.orderId;
+    await Order.deleteOne({ _id: id }).orFail(
+      new NotFound('Order is not found'),
+    );
+    res.status(200).json({
+      message: `Order with this id:${id} deleted`,
+      request: {
+        description: 'To create a new order',
+        type: 'POST',
+        url: `http://localhost:3000/orders`,
+        body: {
+          productId: 'ID',
+          quantity: 'Number',
+        },
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,99 +1,13 @@
 const router = require('express').Router();
-const Order = require('../models/order');
-const Product = require('../models/product');
-const NotFound = require('../../customErrors/NotFound');
 const checkAuth = require('../middlewares/check-auth');
-router.get('/', checkAuth, async (req, res, next) => {
-  try {
-    const orders = await Order.find({})
-      .select('-__v')
-      .populate('product', 'name');
-    const response = {
-      count: orders.length,
-      orders: orders.map((o) => {
-        return {
-          ...o._doc,
-          request: {
-            description: 'Get order details',
-            type: 'GET',
-            url: `http://localhost:3000/orders/${o._id}`,
-          },
-        };
-      }),
-    };
-    res.status(200).json(response);
-  } catch (err) {
-    next(err);
-  }
-});
+const ordersController = require('../controllers/orders');
 
-router.post('/', checkAuth, async (req, res, next) => {
-  try {
-    await Product.findById(req.body.product).orFail(
-      new NotFound('Продукт не найден'),
-    );
-    const { product, quantity } = req.body;
-    const order = await Order.create({
-      product,
-      quantity,
-    });
-    res.status(201).json({
-      message: 'Order was created',
-      order: {
-        ...order._doc,
-        request: {
-          description: 'Get order details',
-          type: 'GET',
-          url: `http://localhost:3000/orders/${order._id}`,
-        },
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+router.get('/', checkAuth, ordersController.orders_get_all);
 
-router.get('/:orderId', checkAuth, async (req, res, next) => {
-  try {
-    const id = req.params.orderId;
-    const order = await Order.findById(id)
-      .select('-__v')
-      .populate('product')
-      .orFail(new NotFound('Order is not found'));
-    res.status(200).json({
-      order,
-      request: {
-        description: 'Get all orders',
-        type: 'GET',
-        url: `http://localhost:3000/orders`,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+router.post('/', checkAuth, ordersController.orders_create_one);
 
-router.delete('/:orderId', checkAuth, async (req, res, next) => {
-  try {
-    const id = req.params.orderId;
-    await Order.deleteOne({ _id: id }).orFail(
-      new NotFound('Order is not found'),
-    );
-    res.status(200).json({
-      message: `Order with this id:${id} deleted`,
-      request: {
-        description: 'To create a new order',
-        type: 'POST',
-        url: `http://localhost:3000/orders`,
-        body: {
-          productId: 'ID',
-          quantity: 'Number',
-        },
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+router.get('/:orderId', checkAuth, ordersController.orders_get_one);
+
+router.delete('/:orderId', checkAuth, ordersController.orders_delete_one);
 
 module.exports = router;
