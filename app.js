b8d071e4d946154a26a7620064b87cf6c09df391
Denis Luchnikov <lasvegas3600@gmail.com>
2021-03-19 10:30:50 +0300

    Add cors middleaware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const config = require('./config');
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 const usersRoutes = require('./api/routes/users');
+const {cors} = require('./api/middlewares/cors')
 const { checkError } = require('./api/middlewares/check-error');
 const { checkWrongImage } = require('./api/middlewares/check-wrong-image');
 const { pageNotFound } = require('./api/middlewares/page-not-found');
@@ -26,18 +27,7 @@ app.use('/uploads', express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    res.status(200).json({});
-  }
-  next();
-});
+app.use(cors);
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/users', usersRoutes);
